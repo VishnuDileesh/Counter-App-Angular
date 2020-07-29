8b73c11fe85ac31cd6aebd295b6cad3918cc590b
Vishnu Dileesh <dev@vishnudileesh.com>
2020-07-29 16:47:30 +0530

    Add displayCounter component functionality

--- src/app/display-counter/display-counter.component.ts
+++ src/app/display-counter/display-counter.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-display-counter',
   templateUrl: './display-counter.component.html',
-  styleUrls: ['./display-counter.component.css']
+  styleUrls: ['./display-counter.component.css'],
 })
 export class DisplayCounterComponent implements OnInit {
+  @Input() counter: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
